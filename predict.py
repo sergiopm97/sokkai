fd60d77047c8612860e0e9a1c581541a329104a7
Sergio Peña Muñoz <92332731+sergiopm97@users.noreply.github.com>
2022-07-04 16:17:58 +0200

    Process data along the pipeline
    Transformation of today's match data using a pipeline containing the processes of the training phase

--- predict.py
+++ predict.py
@@ -1,4 +1,6 @@
+from joblib import load
 import pyfiglet
+import pandas as pd
 from rich.console import Console
 from rich.status import Status
 
@@ -7,6 +9,7 @@ import os
 import time
 
 from download import download_today_matches
+from extract import extract_columns
 
 
 if __name__ == "__main__":
@@ -21,6 +24,9 @@ if __name__ == "__main__":
     with open("config/database_columns.json") as database_columns_config:
         database_columns = json.load(database_columns_config)
 
+    match_columns_plus_info = list(database_columns.values())[0:15]
+    match_features = list(database_columns.values())[6:15]
+
     today_matches_download_status = Status(
         "Downloading soccer matches for today[white]...[/white]"
     )
@@ -34,5 +40,30 @@ if __name__ == "__main__":
 
     console.print(
         "[green][OK][/green] Today soccer matches download done!",
+        style="bold white",
+    )
+
+    pipeline_status = Status("Processing the data along the pipeline[white]...[/white]")
+
+    pipeline_status.start()
+    time.sleep(1)
+
+    matches_data_plus_info = extract_columns(
+        today_soccer_matches, match_columns_plus_info
+    )
+
+    matches_info_data = matches_data_plus_info.drop(columns=match_features)
+    matches_features_data = matches_data_plus_info[match_features]
+
+    scaler = load("models/winner_model/winner_scaler.pkl")
+
+    processed_matches_data = pd.DataFrame(
+        scaler.transform(matches_features_data), columns=matches_features_data.columns
+    )
+
+    pipeline_status.stop()
+
+    console.print(
+        "[green][OK][/green] Data processing along the pipeline done!",
         style="bold white",
     )
